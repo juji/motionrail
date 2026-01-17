d2078cdec19ac43a11f166a17956b409c57ae3a4
juji <jujiyangasli@gmail.com>
2026-01-17 19:19:48 +0700

    Add Svelte framework package with SvelteKit build

---
+++ frameworks/svelte/src/lib/svelte.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+  import { MotionRail as MotionRailClass } from "motionrail";
+  import type { MotionRailOptions } from "motionrail";
+
+  export let options: MotionRailOptions = {};
+  export let instance: MotionRailClass | null = null;
+  export let container: HTMLDivElement | undefined = undefined;
+
+  let containerRef: HTMLDivElement;
+  let motionRailInstance: MotionRailClass | null = null;
+  let slotObserver: MutationObserver | null = null;
+
+  $: instance = motionRailInstance;
+  $: container = containerRef;
+
+  onMount(() => {
+    if (!containerRef) return;
+
+    motionRailInstance = new MotionRailClass(containerRef, options);
+
+    const gridElement = containerRef.querySelector('[data-motionrail-grid]');
+    if (gridElement) {
+      slotObserver = new MutationObserver(() => {
+        if (motionRailInstance) {
+          motionRailInstance.update();
+        }
+      });
+      slotObserver.observe(gridElement, {
+        childList: true,
+        subtree: true,
+      });
+    }
+  });
+
+  $: if (motionRailInstance && options) {
+    if (containerRef) {
+      motionRailInstance.destroy();
+      motionRailInstance = new MotionRailClass(containerRef, options);
+    }
+  }
+
+  onDestroy(() => {
+    if (slotObserver) {
+      slotObserver.disconnect();
+      slotObserver = null;
+    }
+    if (motionRailInstance) {
+      motionRailInstance.destroy();
+      motionRailInstance = null;
+    }
+  });
+</script>
+
+<div bind:this={containerRef} data-motionrail {...$$restProps}>
+  <div data-motionrail-scrollable>
+    <div data-motionrail-grid>
+      <slot />
+    </div>
+  </div>
+</div>
